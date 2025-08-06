09cae9b6299c142fedd88a0de8b763db44c795b6
vah-ram <vahram.s.ghazaryan@gmail.com>
2025-08-06 16:38:44 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+<<<<<<< HEAD
+
+window.addEventListener('load', () => {
+    const topBtn = document.querySelector('.toTopBtn');
+
+    topBtn.addEventListener('click', () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+=======
+<<<<<<< HEAD
+
+window.addEventListener('load', () => {
+    const topBtn = document.querySelector('.toTopBtn');
+
+    topBtn.addEventListener('click', () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+=======
+
+window.addEventListener('load', () => {
+    const topBtn = document.querySelector('.toTopBtn');
+
+    topBtn.addEventListener('click', () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+>>>>>>> 3839a9a371df2f05b24195017c6592d05d3b3c04
+>>>>>>> 2f4044c (Initial commit)
+});
